99f547aec941d68ac247be3c377899fa5b36f1cc
Yue Cao <yue.cao@my.jcu.edu.au>
2021-11-29 23:59:35 +0800

    Refactor checking of password into a function

--- password_check.py
+++ password_check.py
@@ -8,10 +8,14 @@ Password Checker. Created by Yue Cao. 24-11-2021
 def main():
     """Start Program"""
     password = "Caoyue0000"
-    if password == "Caoyue0000":
+    if check_password(password):
         print("Logged in!")
     else:
         print("Password error!")
+
+
+def check_password(password):
+    return password == "Caoyue0000"
 
 
 main()
