cdbde4296acfb1940763e59e7e6f7e9d2169171e
Yue Cao <yue.cao@my.jcu.edu.au>
2021-11-29 23:51:38 +0800

    Add password check program

---
+++ password_check.py
@@ -0,0 +1,17 @@
+"""
+Check Login Credentials.
+
+Password Checker. Created by Yue Cao. 24-11-2021
+"""
+
+
+def main():
+    """Start Program"""
+    password = "Caoyue0000"
+    if password == "Caoyue0000":
+        print("Logged in!")
+    else:
+        print("Password error!")
+
+
+main()
